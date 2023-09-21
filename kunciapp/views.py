862766ec09e2045c2a827abded97cf3e33a79c52
MFathurrohmanHaris <harisfathurrohman142@gmail.com>
2023-09-21 21:11:36 +0700

    membuat views untuk Anggota

--- kunciapp/views.py
+++ kunciapp/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Anggota
+from .serializers import AnggotaSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class AnggotaViewSet(viewsets.ModelViewSet):
+  queryset = Anggota.objects.all()
+  serializer_class = AnggotaSerializer
+  permission_classes = [IsAuthenticated]
+  
+  def create(self, request, *args, **kwargs):
+    serializer = AnggotaSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      response = {
+        'status': 'success',
+        'code': status.HTTP_200_OK,
+        'message': 'Data Anggota Berhasil ditambahkan',
+        'files': serializer.data,
+      }
+      return Response(response)
+    else :
+      response = {
+        'status': 'Bad Request',
+        'code': status.HTTP_400_BAD_REQUEST,
+        'message': 'Harus Login Untuk Menambah Content Ini',
+      }
+      return Response(response)
+    
+  def update(self, request, *args, **kwargs):
+    partial = kwargs.pop('partial', True)
+    instance = self.get_queryset()
+    serializer = AnggotaSerializer(instance, data=request.data, partial=partial)
+    if serializer.is_valid():
+      serializer.save()
+      response = {
+        'status': 'success',
+        'code': status.HTTP_200_OK,
+        'message': 'Anggota Berhasil diperbaharui',
+      }
+      return Response(response)
+    else:
+      response = {
+        'status': 'bad request',
+        'code': status.HTTP_400_BAD_REQUEST,
+        'message': 'Harus Login untuk mengubah data',
+      }
+      return Response(response)
+    
+  def destroy(self, request, *args, **kwargs):
+    instance = self.get_queryset()
+    instance.delete()
+    
+    response = {
+      'status': 'success',
+      'code': status.HTTP_204_NO_CONTENT,
+      'message': 'Anggota Berhasil dihapus'
+    }
+    return Response(response)
